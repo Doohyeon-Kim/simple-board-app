c768d2acf23d5f1d64a8c81482afd83c32b814a0
doohyeon.kim <doohyeon.kim@doolumi.com>
2021-12-13 16:39:38 +0900

    project push

---
+++ dist/boards/boards.service.d.ts
@@ -0,0 +1,9 @@
+import { Board, BoardStatus } from './board.model';
+export declare class BoardsService {
+    private boards;
+    getAllBoards(): Board[];
+    createBoard(createBoardDto: any): Board;
+    getBoardById(id: string): Board;
+    deleteBoardById(id: string): void;
+    updateBoardStatus(id: string, status: BoardStatus): Board;
+}
